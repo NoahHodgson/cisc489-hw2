164341e0c051ef85384bda1a13e01a5fbed0ad9a
Noah <noahmhodgson@gmail.com>
2022-04-18 21:58:24 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Homework / Assignment 2
+
+
+
+## Part 1
+
+Copied the code per the instructions. I didn't run into issues once I commented out the code that made it run
+for too long. Even on my midtier laptop that one function made it heat up. The rest of it ran fine. I had already
+used NLTK for the first assignment, so this was mostly review for me. It was cool to learn about the universal
+POS tagset though. 
+
+## Part 2 
+
+For my test set that I brought in, I did the first page of the Hobbit. I didn't need to tweak much. I did have to add
+besides change an index that was starting at 1 instead of 0. Just going to blame my local data-scientists on that.
+The text analyzed in the expected way. It was pain-free.
+
+## Part 3 
+Trying to decypher the Viterbi problem made me feel out of my depth.So I did what I always do and 
+read some documentations and tutorials. [This](https://www.pythonpool.com/viterbi-algorithm-python/)
+was the most helpful. I based my code mostly on this. I was unable to get the transmission output I desired
+unfortunately, but I did come really close. I was getting Hot Cold Hot Cold Hot Hot or 
+Hot Cold Hot Hot Cold Hot. Those are the right amount of Hots and Cold and the transitions are happening on
+the 2s, just not the right two (last one). I am sure if I had a bit more time to tinker with the values I could
+figure this out, but I am pretty happy with how this turned out.
--- hw2.py
+++ hw2.py
@@ -18,22 +18,21 @@ def main():
     # download the universal tagset from nltk
     nltk.download('universal_tagset')
 
-    #load in the corpus via the reader
+    # load in the corpus via the reader
     root = 'C:/Users/noahm/Desktop/School/Spring 2022/CISC489/nlp-assignment-homework2/text/foo.txt'
     text_file = open(root, "r")
-    #read whole file to a string
+    # read whole file to a string
     data = text_file.read()
-    #close file
+    # close file
     text_file.close()
 
-    sent_text = nltk.sent_tokenize(data) # this gives us a list of sentences
+    sent_text = nltk.sent_tokenize(data)  # this gives us a list of sentences
     # now loop over each sentence and tokenize it separately
     tagged = []
     for sentence in sent_text:
         tokenized_text = nltk.word_tokenize(sentence)
         tagged.append(nltk.pos_tag(tokenized_text, tagset='universal'))
     print(tagged)
-    
 
     # reading the Treebank tagged sentences
     nltk_data = tagged
@@ -127,7 +126,7 @@ def main():
     # define a random seed to get same sentences when run multiple times
     random.seed(1234)
 
-    # choose random 10 numbers
+    # choose random 10 numbers, added a -1 NOAH
     rndom = [random.randint(1, len(test_set)-1) for x in range(10)]
 
     # list of 10 sents on which we test the model
@@ -139,139 +138,154 @@ def main():
     # list of untagged words
     test_tagged_words = [tup[0] for sent in test_run for tup in sent]
 
-    #Here We will only test 10 sentences to check the accuracy
-    #as testing the whole training set takes huge amount of time
+    # Here We will only test 10 sentences to check the accuracy
+    # as testing the whole training set takes huge amount of time
     start = time.time()
     tagged_seq = Viterbi(test_tagged_words)
     end = time.time()
     difference = end-start
-    
+
     print("Time taken in seconds: ", difference)
-    
+
     # accuracy
-    check = [i for i, j in zip(tagged_seq, test_run_base) if i == j] 
-    
+    check = [i for i, j in zip(tagged_seq, test_run_base) if i == j]
+
     accuracy = len(check)/len(tagged_seq)
-    print('Viterbi Algorithm Accuracy: ',accuracy*100)
+    print('Viterbi Algorithm Accuracy: ', accuracy*100)
 
-
-    #To improve the performance,we specify a rule base tagger for unknown words 
+    # To improve the performance,we specify a rule base tagger for unknown words
     # specify patterns for tagging
     patterns = [
         (r'.*ing$', 'VERB'),              # gerund
-        (r'.*ed$', 'VERB'),               # past tense 
-        (r'.*es$', 'VERB'),               # verb    
+        (r'.*ed$', 'VERB'),               # past tense
+        (r'.*es$', 'VERB'),               # verb
         (r'.*\'s$', 'NOUN'),              # possessive nouns
         (r'.*s$', 'NOUN'),                # plural nouns
         (r'\*T?\*?-[0-9]+$', 'X'),        # X
-        (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'), # cardinal numbers
+        (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'),  # cardinal numbers
         (r'.*', 'NOUN')                   # nouns
     ]
-    
+
     # rule based tagger
     rule_based_tagger = nltk.RegexpTagger(patterns)
 
-    #modified Viterbi to include rule based tagger in it
-    def Viterbi_rule_based(words, train_bag = train_tagged_words):
+    # modified Viterbi to include rule based tagger in it
+    def Viterbi_rule_based(words, train_bag=train_tagged_words):
         state = []
         T = list(set([pair[1] for pair in train_bag]))
-        
+
         for key, word in enumerate(words):
-            #initialise list of probability column for a given observation
-            p = [] 
+            # initialise list of probability column for a given observation
+            p = []
             for tag in T:
                 if key == 0:
                     transition_p = tags_df.loc['.', tag]
                 else:
                     transition_p = tags_df.loc[state[-1], tag]
-                    
+
                 # compute emission and state probabilities
-                emission_p = word_given_tag(words[key], tag)[0]/word_given_tag(words[key], tag)[1]
-                state_probability = emission_p * transition_p    
+                emission_p = word_given_tag(words[key], tag)[
+                                            0]/word_given_tag(words[key], tag)[1]
+                state_probability = emission_p * transition_p
                 p.append(state_probability)
-                
+
             pmax = max(p)
-            state_max = rule_based_tagger.tag([word])[0][1]       
-            
-            
-            if(pmax==0):
-                state_max = rule_based_tagger.tag([word])[0][1] # assign based on rule based tagger
+            state_max = rule_based_tagger.tag([word])[0][1]
+
+            if(pmax == 0):
+                # assign based on rule based tagger
+                state_max = rule_based_tagger.tag([word])[0][1]
             else:
                 if state_max != 'X':
                     # getting state for which probability is maximum
-                    state_max = T[p.index(pmax)]                
-                
-            
+                    state_max = T[p.index(pmax)]
+
             state.append(state_max)
         return list(zip(words, state))
-    
-    #test accuracy on subset of test data 
+
+    # test accuracy on subset of test data
     start = time.time()
     tagged_seq = Viterbi_rule_based(test_tagged_words)
     end = time.time()
     difference = end-start
-    
-    print("Time taken in seconds: ", difference)
-    
-    # accuracy
-    check = [i for i, j in zip(tagged_seq, test_run_base) if i == j] 
-    
-    accuracy = len(check)/len(tagged_seq)
-    print('Viterbi Algorithm Accuracy: ',accuracy*100)
 
-    #Check how a sentence is tagged by the two POS taggers
-    #and compare them
-    test_sent="Hobbits name is Fred"
-    pred_tags_rule=Viterbi_rule_based(test_sent.split())
-    pred_tags_withoutRules= Viterbi(test_sent.split())
+    print("Time taken in seconds: ", difference)
+
+    # accuracy
+    check = [i for i, j in zip(tagged_seq, test_run_base) if i == j]
+
+    accuracy = len(check)/len(tagged_seq)
+    print('Viterbi Algorithm Accuracy: ', accuracy*100)
+
+    # Check how a sentence is tagged by the two POS taggers
+    # and compare them
+    test_sent = "Hobbits name is Fred"
+    pred_tags_rule = Viterbi_rule_based(test_sent.split())
+    pred_tags_withoutRules = Viterbi(test_sent.split())
     print(pred_tags_rule)
     print(pred_tags_withoutRules)
-    #Will and Marry are tagged as NUM as they are unknown words for Viterbi Algorithm 
+    # https://www.pythonpool.com/viterbi-algorithm-python/
+
+    observations = ("1", "2", "3", "2", "2", "2")
+    states = ("Hot", "Cold")
+    start_p = {"Hot": 0.6, "Cold": 0.4}
+    trans_p = {
+        "Hot": {"Hot": 0.5, "Cold": 0.5},
+        "Cold": {"Hot": 1, "Cold": 0},
+    }
+    emit_p = {
+        "Hot": {"1": 1, "2": 0.658, "3": 1},
+        "Cold": {"1": 0.0, "2": 0.342, "3": 0.0},
+    }
+    # target H C H H H C H
     def icecream_viterbi(observations, states, start_p, trans_p, emit_p):
         V = [{}]
         for st in states:
-            V[0][st] = {"prob": start_p[st] * emit_p[st][observations[0]], "prev": None}
-    
+            V[0][st] = {"prob": start_p[st] * emit_p[st]
+                [observations[0]], "prev": None}
+
         for t in range(1, len(observations)):
             V.append({})
             for st in states:
-                max_tr_prob = V[t - 1][states[0]]["prob"] * trans_p[states[0]][st]
+                max_tr_prob = V[t - 1][states[0]]["prob"] * \
+                    trans_p[states[0]][st]
                 prev_st_selected = states[0]
                 for prev_st in states[1:]:
                     tr_prob = V[t - 1][prev_st]["prob"] * trans_p[prev_st][st]
                     if tr_prob > max_tr_prob:
                         max_tr_prob = tr_prob
                         prev_st_selected = prev_st
-    
+
                 max_prob = max_tr_prob * emit_p[st][observations[t]]
                 V[t][st] = {"prob": max_prob, "prev": prev_st_selected}
         for line in dptable(V):
             print(line)
-    
+
         opt = []
         max_prob = 0.0
         best_st = None
-    
+
         for st, data in V[-1].items():
             if data["prob"] > max_prob:
                 max_prob = data["prob"]
                 best_st = st
         opt.append(best_st)
         previous = best_st
-    
-    
+
         for t in range(len(V) - 2, -1, -1):
             opt.insert(0, V[t + 1][previous]["prev"])
             previous = V[t + 1][previous]["prev"]
-    
-        print ("The steps of states are " + " ".join(opt) + " with highest probability of %s" % max_prob)
-    
+
+        print("The steps of states are " + " ".join(opt) +
+              " with highest probability of %s" % max_prob)
+
     def dptable(V):
-        
+
         yield " ".join(("%12d" % i) for i in range(len(V)))
         for state in V[0]:
             yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
+    icecream_viterbi(observations, states, start_p, trans_p, emit_p)
 
 if __name__ == '__main__':
     main()
